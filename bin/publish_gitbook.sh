73e9af08d163c4b37b5ff320624e86f466cdc0a0
ken.lj <ken.lj.hz@gmail.com>
2017-08-02 14:13:21 +0800

    MISC: 完善发布脚本

--- bin/publish_gitbook.sh
+++ bin/publish_gitbook.sh
@@ -3,9 +3,9 @@ cd `dirname $0`
 BIN_DIR=`pwd`
 cd ..
 DEPLOY_DIR=`pwd`
-STATIC_TMP_DIR="~/gh-pages-tmp"
+STATIC_TMP_DIR=$HOME/gh-pages-tmp
 
-if [ ! -d ~/gh-pages-tmp ] ; then
+if [ ! -d "$STATIC_TMP_DIR" ] ; then
     git clone -b gh-pages https://github.com/dubbo/dubbo-docs.git $STATIC_TMP_DIR
 fi
 
@@ -30,9 +30,9 @@ filelist=`ls -1`
 for filename in $filelist ; do
 if [ -f "$filename" ] ; then
     echo $filename
-    cp -rf ./$filename $STATIC_TMP_DIR/
+    cp -rf ./$filename ./
 elif [ -d "$filename" ] && [ ! "$filename" == "user-guide" -a ! "$filename" == "developer-guide" -a ! "$filename" == "admin-guide" ] ; then
-   cp -rf ./$filename  $STATIC_TMP_DIR/
+   cp -rf ./$filename  ./
 fi
 done
 
